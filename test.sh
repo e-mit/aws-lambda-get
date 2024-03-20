b38241c2e47386788ff0f8a41ca35181fa8371d4
e <6410762+e-mit@users.noreply.github.com>
2024-03-20 12:14:50 +0000

    Add linting checks

--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+set -x #echo on
 
-python3 test_lambda.py
+python -m pytest -s
+python -m bandit -r . --exclude=/venv/,/package/
+python -m pytest --cov=.
+python -m flake8 --exclude=venv/*,package/*
+python -m mypy . --exclude 'venv/' --exclude 'package/' --explicit-package-bases
+python -m pycodestyle function/*.py
+python -m pydocstyle function/*.py --ignore=D107,D203,D213
+python -m pylint function/*.py
+python -m pyright function/*.py
